74cc091162d7aa4c3cfb900c02a173985df6d775
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-10 09:54:00 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import PageTitle from './components/pageTitle'
 import PageContent from './components/pageContent' 
 import Card from './components/Card'
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
