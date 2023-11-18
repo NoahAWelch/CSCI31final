429789185df61057d8e761a5f4ebe57059497931
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-18 08:48:55 -0800

    Add files via upload

---
+++ src/app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return ( 
+      <div className="p-3 bg-gray-100 shadow-lg text-gray-900 border border-red-300 rounded-lg hover:scale-110 hover:bg-blue-400 bg-gradient-to-r from-blue-300 to-blue-400' grow">
+      <h1 className="text-black">hello</h1>
+       </div>
+       )
+}
