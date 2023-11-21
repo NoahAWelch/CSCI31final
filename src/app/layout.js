66ce0b61206118183ab248e9b976b8a854cc1dec
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-20 19:54:47 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={'${inter.className} bg-gradient-to-r from-blue-300 to-blue-900 text-2xl'}>{children}</body>
-    <footer className="text-center">hello</footer>
+      <body className={'${inter.className} bg-gradient-to-r from-blue-300 to-blue-900 flex flex-col min-h-screen text-2xl'}>{children}</body>
+      <footer className="text-center">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:justify-between">
+      <span className="text-sm text-black bold sm:text-center">© 2023 <a href="#" className="hover:underline text-black bold">CSCI-25™</a>. All
+        Rights Reserved.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-black bold sm:mt-0">
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">Contact</a>
+        </li>
+      </ul>
+    </div>
+      </footer>
     </html>
   )
 }
-
