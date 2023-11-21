ac0a2071becba2b133e65c60e4c0843d3a6ce133
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-20 19:56:24 -0800

    Update page.js

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -2,13 +2,27 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/pageTitle'
 import PageContent from '../components/pageContent'
+import Footer from '../components/Footer'
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
+
+export const revalidate = 0
 
 export default function products() {
+
+  const{ data: pop_culture, error } = supabase.from('pop_culture').select()
+
   return ( 
     <div>
     <Navbar />
     <PageTitle title="Pop Culture" />
-    <PageContent content="Content" />
+    <PageContent content="Content" >
+    {pop_culture && pop_culture.map((pop_cultures, idx) => (
+          <Card key={idx} title={pop_cultures.title} subtitle={pop_cultures.subtitle} description={pop_cultures.description} img={pop_cultures.img}/> 
+        ))} 
+        </PageContent>
   </div>
   )
 }
