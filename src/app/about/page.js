04fa69986c96b9ca1d7afd6845d87e5687e87fa8
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-20 19:57:16 -0800

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,13 +2,27 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/pageTitle'
 import PageContent from '../components/pageContent'
+import Footer from '../components/Footer'
 
-export default function About() {
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
+
+export const revalidate = 0
+
+export default function contact() {
+
+  const{ data: science, error } = supabase.from('science').select()
+
   return ( 
     <div>
     <Navbar />
-    <PageTitle title="Geography" />
-    <PageContent content="Content" />
+    <PageTitle title="Science" />
+    <PageContent content="Content" >
+    {science && science.map((sciences, idx) => (
+          <Card key={idx} title={sciences.title} subtitle={sciences.subtitle} description={sciences.description} img={sciences.img}/> 
+        ))} 
+      </PageContent>
   </div>
   )
 }
