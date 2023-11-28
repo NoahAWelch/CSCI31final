e9c9eb508c69af02ac3838c1e4977f901b06a368
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-27 19:10:55 -0800

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar'
 import PageTitle from './components/pageTitle' 
 import PageContent from './components/pageContent' 
 import Card from './components/Card'
-
+import Footer from './components/Footer'
+import { findCards } from './utils/supabase-client'
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
@@ -13,8 +14,8 @@ export const revalidate = 0
 
 export default async function Home() {
 
-const{ data: cards, error } = await supabase.from('cards').select()
-
+//const{ data: cards, error } = await supabase.from('cards').select()
+const cards = await findCards()
   return ( 
     <div>
       <Navbar />
