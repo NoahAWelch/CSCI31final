64bda924cb2e79eb838d5c20e4151941d25ad9eb
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-27 19:09:58 -0800

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/pageTitle'
 import PageContent from '../components/pageContent'
-
+import CardForm from '../components/CardForm'
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
@@ -22,6 +22,8 @@ export default function About() {
           <Card key={idx} title={geographies.title} subtitle={geographies.subtitle} description={geographies.description} img={geographies.img}/> 
         ))} 
       </PageContent>
+      <CardForm />
   </div>
   )
 }
+
