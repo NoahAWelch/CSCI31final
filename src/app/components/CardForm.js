a54f86abdc868b474daf3017656a60d527918d51
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-11-29 15:27:28 -0800

    Update CardForm.js

--- src/app/components/CardForm.js
+++ src/app/components/CardForm.js
@@ -3,16 +3,17 @@ import Field from "./Field"
 import Label from "./Label"
 import Input from "./Input"
 import { insertCard } from "../utils/supabase-client"
+import { redirect } from "next/navigation"
 
 async function createCard(formData) {
     'use server'
-   // console.log('test')
+  
     const title = formData.get('title')
     const subtitle = formData.get('subtitle')
     const img = formData.get('img')
     const description = formData.get('description')
-    insertCard(title, subtitle, img, description)
-
+    insertCard({title, subtitle, img, description})
+redirect('/')
     // get each field out of formData 
     // call supabase-client insertCard
   }
