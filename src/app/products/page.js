950c54b8f16cac810ecc967d73a92d6e5e7bd494
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-12-04 22:14:05 -0800

    Update page.js

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -3,25 +3,27 @@ import Navbar from '../components/Navbar'
 import PageTitle from '../components/pageTitle'
 import PageContent from '../components/pageContent'
 import Footer from '../components/Footer'
-import Card from '../components/Card'
-
+import PopCultureCard from '../components/PopCultureCard'
+import { findMyPopCards } from '../utils/supabase-client'
 import { createClient } from '@supabase/supabase-js';
+
 
 const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 export const revalidate = 0
 
-export default function products() {
-
-  const{ data: pop_culture, error } = supabase.from('pop_culture').select()
+export default async function Products() {
+  const popCulture = await findMyPopCards()
+  
 
   return ( 
     <div>
     <Navbar />
     <PageTitle title="Pop Culture" />
-    <PageContent content="Content" >
-    {pop_culture && pop_culture.map((pop_cultures, idx) => (
-          <Card key={idx} title={pop_cultures.title} subtitle={pop_cultures.subtitle} description={pop_cultures.description} img={pop_cultures.img}/> 
+    <PageContent content="Content" className="flex  basis-1/3">
+    {popCulture&& popCulture.map((popCultures, idx) => (
+          <PopCultureCard key={idx} Questions={popCultures.Questions} Answer1={popCultures.Answer1} 
+          Answer2={popCultures.Answer2} Answer3={popCultures.Answer3} Answer4={popCultures.Answer4}/> 
         ))} 
         </PageContent>
   </div>
