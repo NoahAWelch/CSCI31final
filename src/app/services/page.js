3dfec569605509d18289386109ce18aedcf08ee4
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-12-04 22:13:20 -0800

    Update page.js

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -3,28 +3,28 @@ import Navbar from '../components/Navbar'
 import PageTitle from '../components/pageTitle'
 import PageContent from '../components/pageContent'
 import Footer from '../components/Footer'
-import Card from '../components/Card'
-
+import HistoryCard from '../components/HistoryCard'
+import { findMyCards } from '../utils/supabase-client'
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 export const revalidate = 0
 
-export default function services() {
+export default async function Services() {
+  const history = await findMyCards()
+  
+    return ( 
+      <div>
+         <Navbar />
+         <PageTitle title="Services" /> 
+      <PageContent content="Content" className="flex  basis-1/3" >
+      {history&& history.map((historyAnswers, idx) => (
+  <HistoryCard key={idx} Question={historyAnswers.Question} Answer1={historyAnswers.Answer1} Answer2={historyAnswers.Answer2}
+  Answer3={historyAnswers.Answer3} Answer4={historyAnswers.Answer4} />     
+))}   
 
-  const{ data: history, error } = supabase.from('history').select()
-
-
-  return ( 
-    <div>
-    <Navbar />
-    <PageTitle title="History" />
-    <PageContent content="Content" >
-    {history && history.map((histories, idx) => (
-          <Card key={idx} title={histories.title} subtitle={histories.subtitle} description={histories.description} img={histories.img}/> 
-        ))}
-        </PageContent>
-  </div>
-  )
-}
+          </PageContent>
+    </div>
+    )
+  }
