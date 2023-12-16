d818c3786ba820254f47b7ff7827554a45f43f50
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-12-15 17:16:56 -0800

    Update page.js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,38 +2,12 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import PageTitle from '../components/pageTitle'
 import PageContent from '../components/pageContent'
-import CardForm from '../components/CardForm'
 import { createClient } from '@supabase/supabase-js';
-import Card from '../components/Card'
-import MyTable from '../components/MyTable'
 import { findCards } from '../utils/supabase-client'
 import { findMyGeoCards } from '../utils/supabase-client'
 import GeographyCard from '../components/GeographyCard'
 
-
 export const revalidate = 0
-
-const cardTableColumns = [
-  {
-      title: 'Title',
-      key: 'title',
-  },
-  {
-      title: 'Subtitle',
-      key: 'subtitle',
-  },
-  {
-      title: 'Img',
-      key: 'img',
-  },
-  {
-      title: 'Description',
-      key: 'description',
-  },
-  ]
-
-
-
 const supabase = createClient('https://nsahzrfqusigvxxhcksh.supabase.co', process.env.SUPABASE_SERVICE_ROLE_KEY)
 
 //export const revalidate = 0
@@ -51,8 +25,6 @@ const cards = await findCards()
           <GeographyCard key={idx} key={idx} Question={geographies.Question} Answer1={geographies.Answer1} Answer2={geographies.Answer2} Answer3={geographies.Answer3} Answer4={geographies.Answer4}/> 
         ))} 
       </PageContent>
-      <CardForm />
-      <MyTable columns = {cardTableColumns} records={cards}/>
   </div>
   )
 }
