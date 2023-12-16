31438b6cb2a37330aad3ea4cb9d97c816e2243bf
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2023-12-15 17:23:19 -0800

    Add files via upload

---
+++ src/app/components/ButtonInputs.js
@@ -0,0 +1,12 @@
+export default function ButtonInputs({ type = 'button', id, onClick }) {
+ // const handleClick = () => {
+   // onClick(answer);
+  //};
+    return <button className="p-2 rounded-lg shadow-lg" type={type} id={id} onClick={onClick}></button>;
+  }
+
+
+ 
+   
+ 
+   
---
+++ src/app/components/GeoCard.js
@@ -0,0 +1,5 @@
+export default function GeoCard({answers}) {
+    return  <div className="p-3 transition-all bg-gray-100 shadow-lg text-gray-900 border border-red-300 rounded-lg hover:scale-110 hover:bg-blue-400 bg-gradient-to-r from-blue-300 to-blue-400' grow">
+     <h1>{answers}</h1>
+     </div>
+ }
